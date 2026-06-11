3a2a509bc0195b7e48bc324206727a48fbaaa973
박진욱 <parkjinuk09@gmail.com>
2026-06-11 17:32:24 +0900

    feat : jwt 인증 및 검증 로직 구현 완료

---
+++ src/main/java/com/example/demo/config/JwtAuthFilter.java
@@ -0,0 +1,77 @@
+package com.example.demo.config;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JwtAuthFilter extends GenericFilter {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));  //서명키 생성
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest)servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse)servletResponse;
+
+        String token = httpServletRequest.getHeader("Authorization");
+        try {
+            if(token != null) {
+                if(!token.substring(0,7).equals("Bearer ")) {
+                    throw new AuthenticationServiceException("Bearer 형식이 아닙니다.");
+                }
+
+                String jwtToken = token.substring(7);
+                Claims claims = Jwts.parser()
+                        .verifyWith(getSigningKey())
+                        .build()
+                        .parseClaimsJws(jwtToken)
+                        .getBody();
+
+
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + claims.get("role")));
+                UserDetails userDetails = new User(claims.getSubject(),"",authorities);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities() );
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
+
+
+
+
+            filterChain.doFilter(servletRequest,servletResponse);
+        } catch (Exception e) {
+            e.printStackTrace();
+            httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+            httpServletResponse.setContentType("application/json");
+            httpServletResponse.getWriter().write("invalid token");
+        }
+
+    }
+}
